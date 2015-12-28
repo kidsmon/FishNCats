c8c5177cf19696cdf3b6da7d03e01877179b8ee1
WonJong <kidsmon@naver.com>
2015-12-28 21:31:45 +0900

    아쿠아리움씬에 StarveBar 이끼 스크립트 약간 만듬

--- Scripts/MossController.cs
+++ Scripts/MossController.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class MossController : MonoBehaviour
 {
     // === 외부 파라미터(Inspector 표시) ===================
-
+    public GameObject Mosses;
+    public GameObject[] Moss;
 
     // === 캐쉬 =========================================
 
 
     // === 내부 파라미터 ==================================
-
+    [System.NonSerialized] public Transform[] MossPositions;
 
     // === 유니티 기본 지원 함수 ===========================
-   
+    void Start()
+    {
+        MossPositions = new Transform[12];
+        for (int i = 0; i < 11; i++)
+        {
+            MossPositions[i].position = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(-4.0f, 4.0f));
+        }
+    }
+
     // === 그외 함수 들 ==================================
 }
